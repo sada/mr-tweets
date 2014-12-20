252d033c5cf5b874dbe1bfa03f2e735d910c7f6c
Akihiro Sada <sada@netlab.jp>
2014-12-20 10:43:18 +0900

    Show twitter's tweets.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,34 @@
 class TweetsController < ApplicationController
+  include ActionController::Live
+
+  def index
+  end
+
+  def stream
+    topics = Settings.twitter.topics
+    response.headers['Content-Type'] = 'text/event-stream; charset=utf-8'
+    sse = SSE.new(response.stream, retry: 300, event: "tweet")
+
+    begin
+      twitter_api_client.filter(:track => topics.join(",")) do |tweet|
+        next if tweet.retweet?
+        sse.write(tweet.attrs)
+      end
+    rescue IOError
+
+    ensure
+      sse.close
+    end
+  end
+
+  private
+
+  def twitter_api_client
+    Twitter::Streaming::Client.new do |config|
+      config.consumer_key = Settings.twitter.consumer_key
+      config.consumer_secret = Settings.twitter.consumer_secret
+      config.access_token = Settings.twitter.access_token
+      config.access_token_secret = Settings.twitter.access_token_secret
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root "tweets#index"
+  get "tweets/stream" => "tweets#stream"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
